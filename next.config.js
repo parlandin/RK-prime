b54b15c591834693c00a71c8fb73aa8e386ed3c0
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-05-12 20:47:35 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['fakestoreapi.com','drive.google.com', 'localhost'],
+        domains: ['fakestoreapi.com','drive.google.com', 'localhost', 'rk-prime.herokuapp.com'],
         imageSizes: [40, 48, 60, 64, 96, 128, 256, 384, 400, 700, 800, 1000],
     },
     compiler: {
