2119f915d2091a510e42541941f81148f34a8cb3
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2023-01-11 11:39:07 -0300

    feat: add .env for host

--- pages/[categorys].jsx
+++ pages/[categorys].jsx
@@ -1,102 +1,109 @@
 import Container from "../components/layout/Container";
 import TitleCategory from "../components/TitleCategory";
-import {Styles} from '../styles/category.style';
+import { Styles } from "../styles/category.style";
 import CardProduct from "../components/CardProduct";
-import { dehydrate, QueryClient, useQuery } from 'react-query';
-import Head from 'next/head';
+import { dehydrate, QueryClient, useQuery } from "react-query";
+import Head from "next/head";
 import Loading from "../components/Loading";
 
+const HOST = process.env.NEXT_PUBLIC_HOST;
 
-export async function getStaticProps(context){
-    const {params} = context
-    const queryClient = new QueryClient()
-   
-    await queryClient.prefetchQuery(`${params.categorys}`, 
-        async () => await (await fetch(`http://localhost:5000/produtos/categoria/${params.categorys}`)).json()
-    )
-    
+export async function getStaticProps(context) {
+  const { params } = context;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    `${params.categorys}`,
+    async () =>
+      await (
+        await fetch(`${HOST}/produtos/categoria/${params.categorys}`)
+      ).json()
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      category: params,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const response = await (await fetch(`${HOST}/produtos/categorias/`)).json();
+  const paths = response.map((category) => {
     return {
-        props: {
-            dehydratedState: dehydrate(queryClient),
-            category: params
-        }
-    }
+      params: {
+        categorys: `${category}`,
+      },
+    };
+  });
+
+  return { paths, fallback: false };
 }
-
-
-export  async function getStaticPaths(){
-    const response = await (await fetch(`http://localhost:5000/produtos/categorias/`)).json()
-    const paths = response.map((category) => {
-        return {
-            params: {
-                categorys: `${category}`
-            }
-        }
-    })
-    
-    return {paths, fallback:false}
-    
-}
-
 
 const Category = (props) => {
-    const {categorys} = props.category
+  const { categorys } = props.category;
 
-    const {data, isLoading} = useQuery(
-        `${categorys}`, 
-        async () => await (await fetch(`http://localhost:5000/produtos/categoria/${categorys}`)).json(), 
-        { initialData: props.dehydratedState, staleTime: 50000 })
-    
-    function trasnformTitleText(title) {
-        const texts = {
-            acessorios: "Acessórios",
-            roupas: "Roupas",
-            tattoos: "Tattoos",
-            acessoriosgeeks: "Acessórios Geeks",
-            cospalyers: "Cospalyers",
-            mangas: "Mangas"
-        }
+  const { data, isLoading } = useQuery(
+    `${categorys}`,
+    async () =>
+      await (await fetch(`${HOST}/produtos/categoria/${categorys}`)).json(),
+    { initialData: props.dehydratedState, staleTime: 50000 }
+  );
 
-        return texts[title] || "essa pagina não existe"
-    }
+  function trasnformTitleText(title) {
+    const texts = {
+      acessorios: "Acessórios",
+      roupas: "Roupas",
+      tattoos: "Tattoos",
+      acessoriosgeeks: "Acessórios Geeks",
+      cospalyers: "Cospalyers",
+      mangas: "Mangas",
+    };
 
-    return (
-        <>
-            <Head>
-                <title>{`${trasnformTitleText(categorys)} | RK Prime`}</title>
-            </Head>
+    return texts[title] || "essa pagina não existe";
+  }
 
-            <Container>
-                <Styles.Wrapper>
-                    <Styles.WrapperTitle>
-                        <TitleCategory title={trasnformTitleText(categorys)}  />
-                    </Styles.WrapperTitle>
+  return (
+    <>
+      <Head>
+        <title>{`${trasnformTitleText(categorys)} | RK Prime`}</title>
+      </Head>
 
-                    {isLoading
-                     ?  <Styles.WrapperLoading> <Loading/> </Styles.WrapperLoading>
-                     :  <Styles.WrapperProducts>
-                            {data.map((product) => {
-                                return (
-                                <Styles.WrapperGeneric key={product._id}>
-                                    <CardProduct 
-                                    img={product.imagens[0]} 
-                                    title={product.nome}
-                                    desc={product.descricao}
-                                    price={product.preco}
-                                    id={product._id}
-                                    customClass="mobile-direct"
-                                    rating={product.nota}/>
-                                    
-                                </Styles.WrapperGeneric>
-                                )
-                            }) }
-                        </Styles.WrapperProducts>
-                    }   
+      <Container>
+        <Styles.Wrapper>
+          <Styles.WrapperTitle>
+            <TitleCategory title={trasnformTitleText(categorys)} />
+          </Styles.WrapperTitle>
 
-                </Styles.Wrapper>
-            </Container>    
-        </>
-    )
-}
+          {isLoading ? (
+            <Styles.WrapperLoading>
+              {" "}
+              <Loading />{" "}
+            </Styles.WrapperLoading>
+          ) : (
+            <Styles.WrapperProducts>
+              {data.map((product) => {
+                return (
+                  <Styles.WrapperGeneric key={product._id}>
+                    <CardProduct
+                      img={product.imagens[0]}
+                      title={product.nome}
+                      desc={product.descricao}
+                      price={product.preco}
+                      id={product._id}
+                      customClass="mobile-direct"
+                      rating={product.nota}
+                    />
+                  </Styles.WrapperGeneric>
+                );
+              })}
+            </Styles.WrapperProducts>
+          )}
+        </Styles.Wrapper>
+      </Container>
+    </>
+  );
+};
 
 export default Category;
--- pages/produto/[productId].jsx
+++ pages/produto/[productId].jsx
@@ -1,87 +1,97 @@
 import Head from "next/head";
-import Container from '../../components/layout/Container';
-import GalleryCarousel from '../../components/GalleryCarousel';
-import SectionCategorys from "../../components/layout/SectionCategorys"
-import {Styles} from '../../styles/produtos.style';
-import DetailsProduct from '../../components/DetailsProduct';
+import Container from "../../components/layout/Container";
+import GalleryCarousel from "../../components/GalleryCarousel";
+import SectionCategorys from "../../components/layout/SectionCategorys";
+import { Styles } from "../../styles/produtos.style";
+import DetailsProduct from "../../components/DetailsProduct";
 import ProductDescription from "../../components/ProductDescription";
-import { dehydrate, QueryClient, useQuery } from 'react-query';
+import { dehydrate, QueryClient, useQuery } from "react-query";
 import Loading from "../../components/Loading";
 
+const HOST = process.env.NEXT_PUBLIC_HOST;
 
-export async function getStaticProps(context){
-    const {params} = context
-    const [ ,id] = params.productId.split("&id=")
-    const queryClient = new QueryClient()
-    
-    await queryClient.prefetchQuery(`${id}`, 
-        async () => await (await fetch(`http://localhost:5000/produtos/${id}`)).json()
-    )
-    
-    return {
-        props: {
-            dehydratedState: dehydrate(queryClient),
-            productId: params
-        }
-    }
+export async function getStaticProps(context) {
+  const { params } = context;
+  const [, id] = params.productId.split("&id=");
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    `${id}`,
+    async () => await (await fetch(`${HOST}/produtos/${id}`)).json()
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      productId: params,
+    },
+  };
 }
 
-export  async function getStaticPaths(){
-    const response = await (await fetch(`http://localhost:5000/produtos/`)).json()
-    const paths = response.map((product) => {
-        return {
-            params: {
-                productId: `${product.nome}&id=${product._id}`
-            }
-        }
-    })
-    return {paths, fallback:false}  
+export async function getStaticPaths() {
+  const response = await (await fetch(`${HOST}/produtos/`)).json();
+  const paths = response.map((product) => {
+    return {
+      params: {
+        productId: `${product.nome}&id=${product._id}`,
+      },
+    };
+  });
+  return { paths, fallback: false };
 }
 
 const Produtos = (props) => {
-    const {productId} = props.productId
-    const [title, id] = productId.split("&id=")
+  const { productId } = props.productId;
+  const [title, id] = productId.split("&id=");
 
-    const {data, isLoading} = useQuery(
-        `${id}`, 
-        async () => await (await fetch(`http://localhost:5000/produtos/${id}`)).json(), 
-        { initialData: props.dehydratedState, staleTime: 50000 })
+  const { data, isLoading } = useQuery(
+    `${id}`,
+    async () => await (await fetch(`${HOST}{/produtos/${id}`)).json(),
+    { initialData: props.dehydratedState, staleTime: 50000 }
+  );
 
-    return (
-        <Container>
-            <Head>
-                <title>{`${title} | RK Prime`}</title>
-            </Head>
-            {isLoading 
-             ? <Loading/>
-             : <>
-                <Styles.Wrapper>
-                    <Styles.ProdutoInfo>
-                        <Styles.WrapperGeneric className="SlideImages">
-                            <GalleryCarousel arrayImages={data.imagens} description={data.descricao}/>
-                        </Styles.WrapperGeneric>
-                        
-                        <Styles.WrapperGeneric>
-                            <DetailsProduct 
-                            title={data.nome} 
-                            price={data.preco}
-                            max={data.quantidade}
-                            description={data.descricao}
-                            id={data._id}
-                            image={data.imagens[0]}/>
-                        </Styles.WrapperGeneric>
-                    
-                    </Styles.ProdutoInfo>
+  return (
+    <Container>
+      <Head>
+        <title>{`${title} | RK Prime`}</title>
+      </Head>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Styles.Wrapper>
+            <Styles.ProdutoInfo>
+              <Styles.WrapperGeneric className="SlideImages">
+                <GalleryCarousel
+                  arrayImages={data.imagens}
+                  description={data.descricao}
+                />
+              </Styles.WrapperGeneric>
 
-                    <SectionCategorys  title={"Itens que podem ser do seu interesse"} categorys="recomended" tags={data.tags}/>
-                    
-                </Styles.Wrapper>
-            
-                <ProductDescription  description={data.descricao}/>
-            </>}
-        </Container>
-        
-    )
-}
+              <Styles.WrapperGeneric>
+                <DetailsProduct
+                  title={data.nome}
+                  price={data.preco}
+                  max={data.quantidade}
+                  description={data.descricao}
+                  id={data._id}
+                  image={data.imagens[0]}
+                />
+              </Styles.WrapperGeneric>
+            </Styles.ProdutoInfo>
+
+            <SectionCategorys
+              title={"Itens que podem ser do seu interesse"}
+              categorys="recomended"
+              tags={data.tags}
+            />
+          </Styles.Wrapper>
+
+          <ProductDescription description={data.descricao} />
+        </>
+      )}
+    </Container>
+  );
+};
 
 export default Produtos;
